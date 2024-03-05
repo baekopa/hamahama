ca169803e9bbffe413943686ec98263833cbe7d6
배성규 <sgbae777@gmail.com>
2024-03-05 22:39:42 +0900

    🎉 FE Project Start
    - 기본 구조 및 라우팅, 일부 페이지 스케치

---
+++ Frontend/src/components/mypage/MyStudy.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>내 스터디 목록 보여주는 컴포넌트일껄?</div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ Frontend/src/components/study/StudyHome.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <p>스터디 홈</p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
