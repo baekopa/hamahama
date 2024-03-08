06067664af70bac4dacb948f8fae4a2c9d17c27a
tddudwns1 <tjddudwns1@naver.com>
2024-03-08 17:20:59 +0900

    ✨ Feat: Common Response 1 - 성공 응답 코드 - api 데이터 모델

---
+++ Backend/src/main/java/com/baekopa/backend/global/response/success/SuccessCode.java
@@ -0,0 +1,52 @@
+package com.baekopa.backend.global.response.success;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessCode {
+
+    /**
+     * ******************************* Success CodeList ***************************************
+     * common HTTP Status Code
+     * 200 : OK                 성공
+     * 201 : Created            생성됨
+     * 202 : Accepted           허용됨
+     * -------------------
+     * other HTTP Status Code
+     * 204 : No Content         콘텐츠 없음
+     * 206 : Partial Content    일부 콘텐츠
+     * *********************************************************************************************
+     */
+    //basic
+    INSERT_SUCCESS(201, "삽입 성공"),
+    SELECT_SUCCESS(200, "조회 성공"),
+    UPDATE_SUCCESS(204, "수정 성공"),
+    DELETE_SUCCESS(204, "삭제 성공"),
+
+    /**
+     * ******************************* Custom Success CodeList ***************************************
+     */
+    // Member
+    REGISTER_SUCCESS(201, "회원가입에 성공하였습니다."),
+    LOGIN_SUCCESS(200, "로그인에 성공하였습니다."),
+    LOGOUT_SUCCESS(200, "로그아웃에 성공하였습니다."),
+    MEMBER_ID_EXIST(200, "회원 id가 이미 존재합니다."),
+    MEMBER_ID_NOT_EXIST(200, "회원 id가 존재하지 않습니다."),
+    MEMBER_FIND_SUCCESS(200, "회원정보 조회에 성공하였습니다."),
+    MEMBER_UPDATE_SUCCESS(200, "회원정보 수정에 성공하였습니다."),
+    MEMBER_DELETE_SUCCESS(200, "회원 탈퇴에 성공하였습니다."),
+    CHECK_MEMBER_OF_JWT(200, "JWT토큰의 멤버 정보조회에 성공했습니다."),
+
+    ; // End
+
+    /**
+     * ******************************* Success Code Field ***************************************
+     */
+    // 성공 코드의 '코드 상태'를 반환한다.
+    private final int status;
+
+    // 성공 코드의 '코드 메시지'를 반환한다.
+    private final String message;
+}
