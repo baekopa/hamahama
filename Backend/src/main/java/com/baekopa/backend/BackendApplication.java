5cbe9dc73633c004d7ca393c000f69476203062f
oistmil <oistmil@gmail.com>
2024-03-12 15:49:06 +0900

    🔧 Build: S3 설정 추가
    - S3 사용 관련 설정 추가
    - BackendApplication 테스트 API 추가

--- Backend/src/main/java/com/baekopa/backend/BackendApplication.java
+++ Backend/src/main/java/com/baekopa/backend/BackendApplication.java
@@ -3,10 +3,19 @@ package com.baekopa.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 @EnableJpaAuditing
 public class BackendApplication {
+
+    // 연결 테스트
+    @GetMapping("/api/hello")
+    String hello() {
+        return "Hello World~";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
