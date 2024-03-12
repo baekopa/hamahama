0a672fbc96d7521085e9d439e943b920a00516ba
Soomin Kim <min120926@gmail.com>
2024-03-12 16:09:00 +0900

    ✨ Feat: 외부 API 요청
    - 소셜 로그인, 회원가입 구현 중~~

---
+++ Backend/src/main/java/com/baekopa/backend/global/oauth/infra/kakao/KakaoApiParams.java
@@ -0,0 +1,26 @@
+package com.baekopa.backend.global.oauth.infra.kakao;
+
+import com.baekopa.backend.global.oauth.dto.OAuthApiParams;
+import com.baekopa.backend.global.oauth.provider.OAuthProvider;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+@Getter
+@NoArgsConstructor
+public class KakaoApiParams implements OAuthApiParams {
+    private String authorizationCode;
+
+    @Override
+    public OAuthProvider oAuthProvider() {
+        return OAuthProvider.KAKAO;
+    }
+
+    @Override
+    public MultiValueMap<String, String> makeBody() {
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("code", authorizationCode);
+        return body;
+    }
+}
---
+++ Backend/src/main/java/com/baekopa/backend/global/oauth/infra/kakao/KakaoInfoResponse.java
@@ -0,0 +1,49 @@
+package com.baekopa.backend.global.oauth.infra.kakao;
+
+import com.baekopa.backend.global.oauth.dto.OAuthInfoResponse;
+import com.baekopa.backend.global.oauth.provider.OAuthProvider;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class KakaoInfoResponse implements OAuthInfoResponse {
+
+    @JsonProperty("kakao_account")
+    private KakaoAccount kakaoAccount;
+
+    @Getter
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    static class KakaoAccount {
+        private KakaoProfile profile;
+        private String email;
+    }
+
+    @Getter
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    static class KakaoProfile {
+        private String nickname;
+        private String image;
+    }
+
+    @Override
+    public String getEmail() {
+        return kakaoAccount.email;
+    }
+
+    @Override
+    public String getNickname() {
+        return kakaoAccount.profile.nickname;
+    }
+
+    @Override
+    public String getImage() {
+        return kakaoAccount.profile.image;
+    }
+
+    @Override
+    public OAuthProvider getOAuthProvider() {
+        return OAuthProvider.KAKAO;
+    }
+}
