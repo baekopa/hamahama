8485e1805ab001ffb81591a41049c04bd7c8bbd5
tddudwns1 <tjddudwns1@naver.com>
2024-03-12 17:56:46 +0900

    🐛 Fix: BusinessException annotation 위치 변경 - @Getter 위치 오류 수정 - 중복 기능 생성자 제거

--- Backend/src/main/java/com/baekopa/backend/global/response/error/exception/BusinessException.java
+++ Backend/src/main/java/com/baekopa/backend/global/response/error/exception/BusinessException.java
@@ -11,21 +11,15 @@ import java.util.List;
 /**
  * 에러를 사용하기 위한 구현체
  */
+@Getter
 public class BusinessException extends RuntimeException {
 
-    @Getter
     private final ErrorCode errorCode;
     private List<ErrorResponse.FieldError> errors = new ArrayList<>();
 
     @Builder
     public BusinessException(ErrorCode errorCode, String message) {
         super(message);
-        this.errorCode = errorCode;
-    }
-
-    @Builder
-    public BusinessException(ErrorCode errorCode) {
-        super(errorCode.getMessage());
         this.errorCode = errorCode;
     }
 
