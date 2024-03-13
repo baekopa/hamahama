23079e11c2c8bd4cc05f68dbad363536143cb0ae
Soomin Kim <min120926@gmail.com>
2024-03-13 15:47:10 +0900

    ✨ Feat: Google 로그인, 회원가입 추가
    - 소셜 로그인 구현 중

---
+++ Backend/src/main/java/com/baekopa/backend/global/oauth/infra/google/GoogleApiParams.java
@@ -0,0 +1,28 @@
+package com.baekopa.backend.global.oauth.infra.google;
+
+import com.baekopa.backend.global.oauth.dto.OAuthApiParams;
+import com.baekopa.backend.global.oauth.provider.OAuthProvider;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.net.URLDecoder;
+
+@Getter
+@NoArgsConstructor
+public class GoogleApiParams implements OAuthApiParams {
+    private String authorizationCode;
+    @Override
+    public OAuthProvider oAuthProvider() {
+        return OAuthProvider.GOOGLE;
+    }
+
+    @Override
+    public MultiValueMap<String, String> makeBody() {
+        authorizationCode = URLDecoder.decode(authorizationCode, java.nio.charset.StandardCharsets.UTF_8);
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("code", authorizationCode);
+        return body;
+    }
+}
