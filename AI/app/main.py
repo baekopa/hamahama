4810073f0c076e058143fff78535656c82fedaff
ajeng518 <elegant518@gmail.com>
2024-03-20 17:42:37 +0900

    ✨ Feat: 긴글 분단별 요약 기능 추가
    Former-commit-id: 372d12f7ea7c8e225083650ad42ae7d48585f118

--- AI/app/main.py
+++ AI/app/main.py
@@ -20,4 +20,4 @@ async def root():
     return {"message": "Hello World"}
 
 if __name__=='__main__':
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
--- AI/app/model/Meeting.py
+++ AI/app/model/Meeting.py
@@ -1,8 +1,6 @@
 from pydantic import BaseModel
 
 class Meeting(BaseModel):
-    meeting_id: str
-    # meeting_title: str
-    # 추가 필드 등
+    meetingText:str
 
 
