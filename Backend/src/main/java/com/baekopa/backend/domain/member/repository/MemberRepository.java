813d179f808e7c68f936b7e3d2076d8527707d62
oistmil <oistmil@gmail.com>
2024-03-21 17:08:25 +0900

    ✨ Feat: 스터디 생성에서 스터디 멤버 초대도 함께 - 스터디 멤버 초대에 대한 내용 - 스터디 생성자를 스터디장으로 만듦 - 스터디 delete 관련 수정

--- Backend/src/main/java/com/baekopa/backend/domain/member/repository/MemberRepository.java
+++ Backend/src/main/java/com/baekopa/backend/domain/member/repository/MemberRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByProviderCode(String providerCode);
+
+    Optional<Member> findByIdAndDeletedAtIsNull(Long id);
 }
---
+++ Backend/src/main/java/com/baekopa/backend/domain/study/entity/StudyMember.java
@@ -0,0 +1,56 @@
+package com.baekopa.backend.domain.study.entity;
+
+import com.baekopa.backend.domain.member.entity.Member;
+import com.baekopa.backend.global.entity.BaseBy;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE study_member SET deleted_at = CONVERT_TZ(NOW(), 'UTC', 'Asia/Seoul') WHERE study_member_id = ?")
+public class StudyMember extends BaseBy {
+
+    public enum StudyMemberType {
+        // 스터디장 / 스터디 멤버 / 초대한 상태
+        STUDY_LEADER, STUDY_MEMBER, INVITATION
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "study_member_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "study_id", nullable = false)
+    private Study study;
+
+    @Column(name = "type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    StudyMemberType type;
+
+    @Builder
+    private StudyMember(Member member, Study study, StudyMemberType type) {
+        this.member = member;
+        this.study = study;
+        this.type = type;
+    }
+
+    // 연관관계 편의 메서드
+    public static StudyMember createStudyMember(Study study, Member member, StudyMemberType type) {
+        return builder().member(member)
+                .study(study)
+                .type(type)
+                .build();
+    }
+
+
+}
