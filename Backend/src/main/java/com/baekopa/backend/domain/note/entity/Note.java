f0e4188219152a98c529692621d7aadca7a2f422
Soomin Kim <min120926@gmail.com>
2024-03-22 10:50:26 +0900

    ✨ Feat: 노트 생성 기능 추가
    - 공부하마 노트 생성
    
    
    Former-commit-id: b0abe3fdd368cf730e006a009eed33604351694c

---
+++ Backend/src/main/java/com/baekopa/backend/domain/note/entity/Note.java
@@ -0,0 +1,46 @@
+package com.baekopa.backend.domain.note.entity;
+
+import com.baekopa.backend.domain.member.entity.Member;
+import com.baekopa.backend.global.entity.BaseBy;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Note extends BaseBy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "note_id")
+    private Long id;
+
+    @Column(name = "title", nullable = false, length = 50)
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @OneToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Builder
+    private Note(String title, String content, Member member) {
+        this.title = title;
+        this.content = content;
+        this.member = member;
+    }
+
+    public static Note of(String title, String content, Member member) {
+        return Note.builder()
+                .title(title)
+                .content(content)
+                .member(member)
+                .build();
+    }
+
+}
