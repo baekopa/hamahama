cb3a8c1e360e0162dd66f76a00146bbee061d6a6
oistmil <oistmil@gmail.com>
2024-03-22 11:47:29 +0900

    ♻️ Refactor: Swagger로 api 명세 추가
    Former-commit-id: 89dc9d999ed3e0ddfc8ada074243948a170650ce

--- Backend/src/main/java/com/baekopa/backend/domain/study/controller/StudyMemberController.java
+++ Backend/src/main/java/com/baekopa/backend/domain/study/controller/StudyMemberController.java
@@ -5,6 +5,7 @@ import com.baekopa.backend.domain.member.entity.dto.response.MemberResponseDto;
 import com.baekopa.backend.domain.study.service.StudyMemberService;
 import com.baekopa.backend.global.response.success.ApiResponse;
 import com.baekopa.backend.global.response.success.SuccessCode;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +22,7 @@ public class StudyMemberController {
 
     private final StudyMemberService studyMemberService;
 
+    @Operation(summary = "이메일로 멤버(사용자) 조회", description = "이메일에 쿼리 내용이 포함되는 멤버(사용자)를 조회합니다.")
     @GetMapping("/members")
     public ApiResponse<List<MemberResponseDto>> searchMembers(@RequestParam(value = "q") String query, @AuthenticationPrincipal Member member) {
         return ApiResponse.of(SuccessCode.SELECT_SUCCESS, studyMemberService.searchMembers(query));
