fc8064c2b25b654726df612816340dea5233566c
Soomin Kim <min120926@gmail.com>
2024-03-25 12:57:09 +0900

    🐛 Fix: 노트 생성 수정
    - 노트 생성 수정
    - Meeting entity 생성
    - submittedNote 생성
    - rest template config 생성

---
+++ Backend/src/main/java/com/baekopa/backend/domain/note/entity/SubmittedNote.java
@@ -0,0 +1,52 @@
+package com.baekopa.backend.domain.note.entity;
+
+import com.baekopa.backend.domain.meeting.entity.Meeting;
+import com.baekopa.backend.global.entity.BaseBy;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SubmittedNote extends BaseBy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "submitted_id")
+    private Long id;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "difference_content")
+    private String differenceContent;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "note_id", nullable = false)
+    private Note note;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "meeting_id", nullable = false)
+    private Meeting meeting;
+
+    @Builder
+    private SubmittedNote(String content, String differenceContent, Note note, Meeting meeting) {
+        this.content = content;
+        this.differenceContent = differenceContent;
+        this.note = note;
+        this.meeting = meeting;
+    }
+
+    // 연관관계 편의 메서드
+    public static SubmittedNote createSubmittedNote(String content, String differenceContent, Note note, Meeting meeting) {
+        return builder().content(content)
+                .differenceContent(differenceContent)
+                .note(note)
+                .meeting(meeting)
+                .build();
+    }
+
+}
