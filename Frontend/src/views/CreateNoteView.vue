311527dbad9c238d7fc400b9ee4f4dff3c21ba1c
배성규 <sgbae777@gmail.com>
2024-03-25 16:28:44 +0900

    ✨ Feat : 노트 공유기능 추가중
    - develop pull 받기위해 임시 커밋

--- Frontend/src/views/CreateNoteView.vue
+++ Frontend/src/views/CreateNoteView.vue
@@ -23,22 +23,39 @@ import instance from '@/api/index'
 import { useRouter } from 'vue-router'
 import Swal from 'sweetalert2'
 
-const api = instance()
 const router = useRouter()
 const title = ref('')
 const content = ref('')
 
+const studyList = ref([])
+
 function share() {
   if (title.value === '') {
-    alert('제목은 필수 내용은 선택')
+    Swal.fire({
+      title: '작성한 내용이 없어요',
+      text: '제목은 필수 내용은 선택!',
+      icon: 'question'
+    })
   } else {
-    //여기에 어디로 보낼지 모달 만들어서 해야할듯
+    instance
+      .get('api/members/meetings')
+      .then((res) => {
+        studyList.value = res.data
+        ShareNote()
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   }
 }
 
 function CreateNote() {
   if (title.value === '') {
-    alert('제목은 필수 내용은 선택')
+    Swal.fire({
+      title: '작성한 내용이 없어요',
+      text: '제목은 필수 내용은 선택!',
+      icon: 'question'
+    })
   } else {
     Swal.fire({
       title: '노트를 저장하시겠습니까?',
@@ -47,7 +64,7 @@ function CreateNote() {
     }).then((result) => {
       /* Read more about isConfirmed, isDenied below */
       if (result.isConfirmed) {
-        api
+        instance
           .post(
             `api/notes`,
             {
@@ -77,6 +94,41 @@ function CreateNote() {
           })
       }
     })
+  }
+}
+
+async function ShareNote() {
+  const { value: study } = await Swal.fire({
+    title: '스터디로 내보내기 하마하마?',
+    input: 'select',
+    inputOptions: {
+      Fruits: {
+        apples: 'Apples',
+        bananas: 'Bananas',
+        grapes: 'Grapes',
+        oranges: 'Oranges'
+      },
+      Vegetables: {
+        potato: 'Potato',
+        broccoli: 'Broccoli',
+        carrot: 'Carrot'
+      },
+      icecream: 'Ice cream'
+    },
+    inputPlaceholder: '스터디 모임 전에 내용을 공유해보세요!',
+    showCancelButton: true,
+    inputValidator: (value) => {
+      return new Promise((resolve) => {
+        if (value) {
+          resolve()
+        } else {
+          resolve('You need to select oranges :)')
+        }
+      })
+    }
+  })
+  if (study) {
+    Swal.fire(`You selected: ${study}`)
   }
 }
 </script>
