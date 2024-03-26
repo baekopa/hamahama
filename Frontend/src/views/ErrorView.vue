394776e8e271bac36bbfb91233b34d4f7bfdaf11
oistmil <oistmil@gmail.com>
2024-03-27 00:55:35 +0900

    💄 Design: mypage 디자인 변경 - mypage 사이드바 변경 - mypage 대시보드 및 스터디 변경 - Errorpage 세부 변경 - tossface 적용

--- Frontend/src/views/ErrorView.vue
+++ Frontend/src/views/ErrorView.vue
@@ -2,11 +2,11 @@
   <div>
       <div class="w-full">
         <div class="">
-          <h1 style="" class="error-type text-center font-extrabold">404</h1>
+          <h1 style="" class="error-type text-center font-extrabold point-font italic">404</h1>
         </div>
         <div class="error-msg">
-          <h2 class="error-text text-5xl mb-4 font-bold">요청하신 내용을 찾을 수 없어요!</h2>
-          <h3 class="error-text text-2xl">입력하신 주소가 정확한지 다시 한번 확인해주세요.</h3>
+          <h2 class="error-text text-5xl mb-6 point-font">요청하신 내용을 찾을 수 없어요!</h2>
+          <h3 class="error-text text-2xl point-font">입력하신 주소가 정확한지 다시 한번 확인해주세요.</h3>
         </div>  
         <div class="error-image">
           <img src="../assets/image/error.png" alt="" width="600px" /><img />
@@ -23,14 +23,13 @@
 <style scoped>
 .error-type {
   position:fixed;
-  margin-top: -20px;
-  margin-left: 380px;
+  margin-top: -35px;
+  margin-left: 320px;
   font-size: 500px;
   color: #FF8990;
   opacity: 0.6;
-  /* -webkit-text-stroke-width: 10px; */
+  -webkit-text-stroke-width:8px;
   -webkit-text-stroke-color: #FF8990;
-  font-family: "UhBeeSe_hyun";
 }
 
 .error-image {
@@ -42,7 +41,7 @@
 
 .error-msg {
   position: fixed;
-  margin-top: 630px;
+  margin-top: 610px;
   margin-left: 620px;
 }
 
