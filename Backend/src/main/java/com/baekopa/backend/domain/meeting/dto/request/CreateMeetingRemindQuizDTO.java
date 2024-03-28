c3f33e7a80e6ee29653f8e2601c5878b01ff241a
여아정 <yaj518@naver.com>
2024-03-28 14:08:53 +0900

    ✨ Feat: 미팅내용 리마인드 퀴즈 생성 및 재생성 기능 추가
    - 미팅 내용 리마인드 퀴즈 생성
    - 리마인드 퀴즈 재생성
    
    
    Former-commit-id: 306c4b0ff2ae1fa6c7d45b54233f74897931d61d

---
+++ Backend/src/main/java/com/baekopa/backend/domain/meeting/dto/request/CreateMeetingRemindQuizDTO.java
@@ -0,0 +1,29 @@
+package com.baekopa.backend.domain.meeting.dto.request;
+
+import com.baekopa.backend.domain.meeting.entity.MeetingSummary;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateMeetingRemindQuizDTO {
+    private String summaryText;
+
+    @Builder
+    private CreateMeetingRemindQuizDTO(String summaryText) {
+        this.summaryText = summaryText;
+    }
+
+    public static CreateMeetingRemindQuizDTO of(String summaryText){
+        return builder()
+                .summaryText(summaryText)
+                .build();
+    }
+
+    public static CreateMeetingRemindQuizDTO from(MeetingSummary meetingSummary){
+        return builder()
+                .summaryText(meetingSummary.getSummaryContent())
+                .build();
+    }
+}
