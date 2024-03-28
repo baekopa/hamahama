582d6a7b8180955fdcc63f242b3b03e932c22aa7
Soomin Kim <min120926@gmail.com>
2024-03-28 12:45:59 +0900

    ✨ Feat: 제출된 개인 요약을 요약하기 위한 스케쥴러 설정
    - 스케쥴러 생성
    - 제출된 개인 요약을 요약하기 위한 로직 구현
    
    
    Former-commit-id: 9c3b34fc187c2abb6118e2648236d6433c03a2c2

--- Backend/src/main/java/com/baekopa/backend/domain/note/repository/SubmittedNoteRepository.java
+++ Backend/src/main/java/com/baekopa/backend/domain/note/repository/SubmittedNoteRepository.java
@@ -2,13 +2,17 @@ package com.baekopa.backend.domain.note.repository;
 
 import com.baekopa.backend.domain.meeting.entity.Meeting;
 import com.baekopa.backend.domain.note.entity.Note;
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.baekopa.backend.domain.note.entity.SubmittedNote;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface SubmittedNoteRepository extends JpaRepository<SubmittedNote, Long> {
+
     List<SubmittedNote> findMeetingByNote(Note note);
 
     boolean existsByNoteAndMeeting(Note existNote, Meeting existMeeting);
+
+    List<SubmittedNote> findAllByMeetingAndDeletedAtIsNull(Meeting meeting);
+
 }
