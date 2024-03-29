04674d3807eebf281235bbb41ba25b209d76416d
tddudwns1 <tjddudwns1@naver.com>
2024-03-29 15:13:13 +0900

    🐛 Fix: SSE event 전송 로직 수정 및 제거 로직 추가 1. event 전송 로직 수정 - 수신자가 여러 명일 때를 고려하지 못한 기존 로직 수정 2. event 삭제 로직 구현 - event cache에 저장된 단일 event 제거 함수가 없어 누적되던 기존 로직에서 제거 로직 추가
    Former-commit-id: d3369618f8661948fa9020d1e1fac897752ab77b

--- Backend/src/main/java/com/baekopa/backend/domain/notification/dto/NotificationDto.java
+++ Backend/src/main/java/com/baekopa/backend/domain/notification/dto/NotificationDto.java
@@ -15,27 +15,33 @@ public class NotificationDto {
     private Member receiver;
     private NotificationType notificationType;
     private String notificationContent;
+    private String eventId;
     private Long relatedContentId;
 
     @Builder
-    private NotificationDto(Member receiver, NotificationType notificationType, String notificationContent, Long relatedContentId) {
+    private NotificationDto(Member receiver, NotificationType notificationType, String notificationContent, String eventId, Long relatedContentId) {
         this.receiver = receiver;
         this.notificationType = notificationType;
         this.notificationContent = notificationContent;
+        this.eventId = eventId;
         this.relatedContentId = relatedContentId;
     }
 
-    public static NotificationDto of(Member receiver, NotificationType notificationType, String notificationContent, Long relatedContentId) {
+    public static NotificationDto of(Member receiver, NotificationType notificationType, String notificationContent, String eventId, Long relatedContentId) {
         return builder()
                 .receiver(receiver)
                 .notificationType(notificationType)
                 .notificationContent(notificationContent)
+                .eventId(eventId)
                 .relatedContentId(relatedContentId)
                 .build();
     }
 
     public Notification toEntity() {
-        return Notification.of(receiver, notificationType, notificationContent, relatedContentId);
+        return Notification.of(receiver, notificationType, notificationContent, eventId, relatedContentId);
     }
 
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
 }
