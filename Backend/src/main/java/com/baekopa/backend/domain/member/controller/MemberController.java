eb2d8a6d859d4f6a3fefec57abe016e3db494e42
Soomin Kim <min120926@gmail.com>
2024-03-29 15:42:39 +0900

    ✨ Feat: 스터디 리마인드 퀴즈 목록 조회
    - 스터디 페이지 내에서 확인 가능
    - 스터디에서 생성한 리마인드 퀴즈 조회
    
    
    Former-commit-id: 10106d38cc104f1aaac2fc84978c9d774d2d26d9

--- Backend/src/main/java/com/baekopa/backend/domain/member/controller/MemberController.java
+++ Backend/src/main/java/com/baekopa/backend/domain/member/controller/MemberController.java
@@ -1,6 +1,6 @@
 package com.baekopa.backend.domain.member.controller;
 
-import com.baekopa.backend.domain.meeting.dto.request.MyRemindQuizResponseDto;
+import com.baekopa.backend.domain.meeting.dto.response.RemindQuizResponseDto;
 import com.baekopa.backend.domain.meeting.dto.response.StudyMeetingListDto;
 import com.baekopa.backend.domain.member.dto.request.MyInfoReqeustDto;
 import com.baekopa.backend.domain.member.dto.response.MemberMainResponseDto;
@@ -87,7 +87,7 @@ public class MemberController {
 
     @Operation(summary = "내가 속한 스터디의 리마인드 퀴즈 목록 조회", description = "사용자가 속한 스터디의 리마인드 퀴즈 목록 조회")
     @GetMapping("/remind-quiz")
-    public ApiResponse<List<MyRemindQuizResponseDto>> getMyRemindQuiz(@AuthenticationPrincipal Member member) {
+    public ApiResponse<List<RemindQuizResponseDto>> getMyRemindQuiz(@AuthenticationPrincipal Member member) {
 
         log.info("내가 속한 스터디의 리마인드 퀴즈 목록 조회 : {}", member.getName());
         return ApiResponse.of(SuccessCode.REMIND_QUIZ_GET_SUCCESS, memberService.getMyRemindQuiz(member));
