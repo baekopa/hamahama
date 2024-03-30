de716b581cc2f867b009650f29fdfb369ad4d04a
Soomin Kim <min120926@gmail.com>
2024-03-30 12:23:52 +0900

    🐛 Fix: 스터디 대시보드 수정
    - 반환 dto에 전체요약과 꼬리질문 추가
    
    
    Former-commit-id: b429fbc130b796cb451dbfc6e7e0acf4bf3a7745

--- Backend/src/main/java/com/baekopa/backend/domain/study/dto/response/StudyMeetingResponseDto.java
+++ Backend/src/main/java/com/baekopa/backend/domain/study/dto/response/StudyMeetingResponseDto.java
@@ -18,20 +18,23 @@ public class StudyMeetingResponseDto {
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime studyAt;
     private List<SubmittedNoteDto> submittedNotes;
+    private String entireSummary;
 
     @Builder
-    private StudyMeetingResponseDto(Long id, String topic, LocalDateTime studyAt, List<SubmittedNoteDto> submittedNotes) {
+    private StudyMeetingResponseDto(Long id, String topic, LocalDateTime studyAt, List<SubmittedNoteDto> submittedNotes, String entireSummary) {
         this.id = id;
         this.topic = topic;
         this.studyAt = studyAt;
         this.submittedNotes = submittedNotes;
+        this.entireSummary = entireSummary;
     }
 
-    public static StudyMeetingResponseDto of(Long id, String topic, LocalDateTime studyAt, List<SubmittedNoteDto> submittedNotes) {
+    public static StudyMeetingResponseDto of(Long id, String topic, LocalDateTime studyAt, List<SubmittedNoteDto> submittedNotes, String entireSummary) {
         return builder().id(id)
                 .topic(topic)
                 .studyAt(studyAt)
                 .submittedNotes(submittedNotes)
+                .entireSummary(entireSummary)
                 .build();
     }
 
