c01ec35d07b2be7059dd367c8605af8b9eeab701
tddudwns1 <tjddudwns1@naver.com>
2024-04-02 17:49:13 +0900

    ✨ Feat: 스케줄링 알림 구현, 알림 저장 확인 변수 시간으로 변경, 미확인 알림 목록 여부 로직 변경, 미사용 로직 제거

--- Backend/src/main/java/com/baekopa/backend/domain/meeting/repository/RemindQuizRepository.java
+++ Backend/src/main/java/com/baekopa/backend/domain/meeting/repository/RemindQuizRepository.java
@@ -3,12 +3,17 @@ package com.baekopa.backend.domain.meeting.repository;
 import com.baekopa.backend.domain.meeting.entity.Meeting;
 import com.baekopa.backend.domain.meeting.entity.RemindQuiz;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RemindQuizRepository extends JpaRepository<RemindQuiz, Long> {
     Optional<RemindQuiz> findByMeetingAndDeletedAtIsNull(Meeting meeting);
 
     Optional<RemindQuiz> findByIdAndDeletedAtIsNull(Long remindQuizId);
+
+    @Query(value = "SELECT * FROM remind_quiz WHERE abs(TIMESTAMPDIFF(MINUTE, open_date, now())) <= 1 AND deleted_at is NULL AND open_date >= now() ", nativeQuery = true)
+    List<RemindQuiz> findUpcomingRemindQuizzes();
 
 }
