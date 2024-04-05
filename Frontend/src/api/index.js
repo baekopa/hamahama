cbdd4318dfa11ae63f281e4fad3ad9f1d260b7d9
배성규 <sgbae777@gmail.com>
2024-04-05 10:54:05 +0900

    🧪 reissue

--- Frontend/src/api/index.js
+++ Frontend/src/api/index.js
@@ -13,7 +13,6 @@ const instance = axios.create({
 
 instance.interceptors.request.use(
   async (config) => {
-    // config.headers['Content-Type'] = 'application/json'
     config.headers['Authorization'] = `Bearer ${localStorage.getItem('accessToken')}`
     return config
   },
@@ -27,48 +26,42 @@ async function TokenRefresh() {
   const accessToken = localStorage.getItem('accessToken')
   if (accessToken == null || !accessToken) {
     router.push({ name: 'login' })
-  }
-  try {
-    console.log('reissue보냄')
-    const response = await instance.post('/reissue')
-    console.log('res_data', response.data)
-    console.log('res', response)
-    const newAccessToken = document.cookie.match(/Authorization=([^;]+)/) // 새로운 액세스 토큰
-    if (newAccessToken) {
-      localStorage.setItem('accessToken', newAccessToken[1]) // 로컬 스토리지에 새로운 액세스 토큰 저장
-      document.cookie = 'Authorization=; expires=Thu, 01 Jan 1970 00:00:01 GMT;'
-      sessionStorage.setItem('isLoginHAMAHAMA', true)
-    } else {
-      authStore.isLogin = false
-      localStorage.clear
-      sessionStorage.clear
+  } else {
+    try {
+      console.log('reissue보냄')
+      const response = await instance.post('/reissue')
+      console.log('res_data', response.data)
+      console.log('res', response)
+      const newAccessToken = document.cookie.match(/Authorization=([^;]+)/) // 새로운 액세스 토큰
+      if (newAccessToken) {
+        localStorage.setItem('accessToken', newAccessToken[1]) // 로컬 스토리지에 새로운 액세스 토큰 저장
+        document.cookie = 'Authorization=; expires=Thu, 01 Jan 1970 00:00:01 GMT;'
+        sessionStorage.setItem('isLoginHAMAHAMA', true)
+      } else {
+        authStore.isLogin = false
+        localStorage.clear
+        sessionStorage.clear
+      }
+    } catch (error) {
+      router.push({ name: 'login' }) // 토큰 갱신에 실패한 경우 로그인 페이지로 이동
+      console.error('Token refresh failed:', error)
+      throw error
     }
-  } catch (error) {
-    router.push({ name: 'login' }) // 토큰 갱신에 실패한 경우 로그인 페이지로 이동
-    console.error('Token refresh failed:', error)
-    throw error
   }
 }
 
 instance.interceptors.response.use(
   (response) => {
-    if (response.status === 404) {
-      router.push({ name: 'notFound' })
-    }
     return response
   },
   async (error) => {
     console.log(error)
-    const originalConfig = error.config
-    if (error.response?.status === 401 && !originalConfig._isRetry) {
-      originalConfig._isRetry = true
-      try {
-        await TokenRefresh()
-        return instance(originalConfig)
-      } catch (error) {
-        return Promise.reject(error)
-      }
-    }
+    const { config, response } = error
+
+    const reissue = await TokenRefresh()
+    console.log(reissue)
+
+    return instance(config)
   }
 )
 
