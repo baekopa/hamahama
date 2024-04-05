8ef99e6ca8b0fa23f39b32dd992c51be1cf674b4
배성규 <sgbae777@gmail.com>
2024-04-05 10:07:46 +0900

    🧪

--- Frontend/src/api/index.js
+++ Frontend/src/api/index.js
@@ -50,17 +50,9 @@ instance.interceptors.response.use(
 
   async (error) => {
     console.log(error)
-    if (error.response?.status == 401) {
-      const reissue = await instance.post('/reissue', { withCredentials: true })
-      console.log('reissue 끝 토큰세팅 시작')
-      console.log(reissue)
-      const newAccessToken = document.cookie.match(/Authorization=([^;]+)/)
-      localStorage.setItem('accessToken', newAccessToken[1]) // 로컬 스토리지에 액세스 토큰 저장
-      document.cookie = 'Authorization=; expires=Thu, 01 Jan 1970 00:00:01 GMT;'
-      const accessToken = localStorage.getItem('accessToken')
-      error.config.headers = {
-        Authorization: `Bearer ${accessToken}`
-      }
+    const originalConfig = error.config
+    if (error.response?.status == 401 && originalConfig.retry) {
+      await tokenRefresh()
       const response = await instance.request(originalConfig)
       return response
     }
