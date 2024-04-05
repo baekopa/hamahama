56ff53e5ab0f38db156ab211f8a6ca9469814474
배성규 <sgbae777@gmail.com>
2024-04-05 10:40:20 +0900

    🧪 reissue test
    Former-commit-id: 980e0b854a0431c6b4a78f63cae70236d910c56e

--- Frontend/src/api/index.js
+++ Frontend/src/api/index.js
@@ -30,34 +30,23 @@ async function TokenRefresh() {
   }
   try {
     console.log('reissue보냄')
-    const response = await instance.post('/reissue', { withCredentials: true })
+    const response = await instance.post('/reissue')
     console.log('res_data', response.data)
     console.log('res', response)
     const newAccessToken = document.cookie.match(/Authorization=([^;]+)/) // 새로운 액세스 토큰
-    localStorage.setItem('accessToken', newAccessToken) // 로컬 스토리지에 새로운 액세스 토큰 저장
+    if (newAccessToken) {
+      localStorage.setItem('accessToken', newAccessToken[1]) // 로컬 스토리지에 새로운 액세스 토큰 저장
+      document.cookie = 'Authorization=; expires=Thu, 01 Jan 1970 00:00:01 GMT;'
+      sessionStorage.setItem('isLoginHAMAHAMA', true)
+    } else {
+      authStore.isLogin = false
+      localStorage.clear
+      sessionStorage.clear
+    }
   } catch (error) {
     router.push({ name: 'login' }) // 토큰 갱신에 실패한 경우 로그인 페이지로 이동
     console.error('Token refresh failed:', error)
     throw error
-  }
-}
-
-const SetAccessToken = () => {
-  loadStore.isLoading = true
-  const authorizationCookie = document.cookie.match(/Authorization=([^;]+)/)
-
-  if (authorizationCookie) {
-    localStorage.setItem('accessToken', authorizationCookie[1])
-    document.cookie = 'Authorization=; expires=Thu, 01 Jan 1970 00:00:01 GMT;'
-    sessionStorage.setItem('isLoginHAMAHAMA', true)
-    authStore.isLogin = true
-    loadStore.isLoading = false
-    setTimeout(() => {
-      loadStore.isLoading = false
-      router.push({ name: 'main' })
-    }, 1000)
-  } else {
-    loadStore.isLoading = false
   }
 }
 
@@ -80,7 +69,6 @@ instance.interceptors.response.use(
         return Promise.reject(error)
       }
     }
-    return Promise.reject(error)
   }
 )
 
