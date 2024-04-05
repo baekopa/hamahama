9e905ceb165ea7733ee65c4f831e1327ede47c86
배성규 <sgbae777@gmail.com>
2024-04-05 10:16:09 +0900

    🧪 Reissue Test
    Former-commit-id: 697cdb560e4b5a943b4c8e1b0855512a4b7c44f7

--- Frontend/src/api/index.js
+++ Frontend/src/api/index.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
+
 const isTwice = ref(false)
 const router = useRouter()
+
 const instance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   timeout: 10000,
@@ -10,7 +12,7 @@ const instance = axios.create({
 })
 
 instance.interceptors.request.use(
-  (config) => {
+  async (config) => {
     // config.headers['Content-Type'] = 'application/json'
     config.headers['Authorization'] = `Bearer ${localStorage.getItem('accessToken')}`
     return config
@@ -31,12 +33,14 @@ const tokenRefresh = async () => {
     const response = await instance.post('/reissue', { withCredentials: true })
     console.log('res_data', response.data)
     console.log('res', response)
-    const newAccessToken = document.cookie.match(/Authorization=([^;]+)/)
-    localStorage.setItem('accessToken', newAccessToken[1]) // 로컬 스토리지에 액세스 토큰 저장
-    document.cookie = 'Authorization=; expires=Thu, 01 Jan 1970 00:00:01 GMT;'
+    const newAccessToken = response.data.accessToken // 새로운 액세스 토큰
+    localStorage.setItem('accessToken', newAccessToken) // 로컬 스토리지에 새로운 액세스 토큰 저장
+    instance.defaults.headers.common['Authorization'] = `Bearer ${newAccessToken}` // 새로운 토큰 설정
+    return response
   } catch (error) {
     router.push({ name: 'login' }) // 토큰 갱신에 실패한 경우 로그인 페이지로 이동
     console.error('Token refresh failed:', error)
+    throw error
   }
 }
 
@@ -47,14 +51,17 @@ instance.interceptors.response.use(
     }
     return response
   },
-
   async (error) => {
     console.log(error)
     const originalConfig = error.config
-    if (error.response?.status == 401 && originalConfig.retry) {
-      await tokenRefresh()
-      const response = await instance.request(originalConfig)
-      return response
+    if (error.response?.status === 401 && !originalConfig._isRetry) {
+      originalConfig._isRetry = true
+      try {
+        await tokenRefresh()
+        return instance(originalConfig)
+      } catch (error) {
+        return Promise.reject(error)
+      }
     }
     return Promise.reject(error)
   }
